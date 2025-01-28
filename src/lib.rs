c72bc235f31d1193cfb85962f2ae22386c001946
Hugh Kaznowski <hugh@rapidrecast.io>
2025-01-28 21:57:10 +0000

    Fixup

--- src/lib.rs
+++ src/lib.rs
@@ -1,89 +1,89 @@
 #[cfg(feature = "dtls-client")]
-pub use proto_dtls::client as proto_dtls_client;
+pub use proto_tower_dtls::client as proto_tower_dtls_client;
 #[cfg(feature = "dtls-data")]
-pub use proto_dtls::data as proto_dtls_data;
+pub use proto_tower_dtls::data as proto_tower_dtls_data;
 #[cfg(feature = "dtls-server")]
-pub use proto_dtls::server as proto_dtls_server;
+pub use proto_tower_dtls::server as proto_tower_dtls_server;
 #[cfg(feature = "grpc-client")]
-pub use proto_grpc::client as proto_grpc_client;
+pub use proto_tower_grpc::client as proto_tower_grpc_client;
 #[cfg(feature = "grpc-data")]
-pub use proto_grpc::data as proto_grpc_data;
+pub use proto_tower_grpc::data as proto_tower_grpc_data;
 #[cfg(feature = "grpc-server")]
-pub use proto_grpc::server as proto_grpc_server;
+pub use proto_tower_grpc::server as proto_tower_grpc_server;
 
 #[cfg(feature = "http-1-client")]
-pub use proto_http_1::client as proto_http_1_client;
+pub use proto_tower_http_1::client as proto_tower_http_1_client;
 #[cfg(feature = "http-1-data")]
-pub use proto_http_1::data as proto_http_1_data;
+pub use proto_tower_http_1::data as proto_tower_http_1_data;
 #[cfg(feature = "http-1-server")]
-pub use proto_http_1::server as proto_http_1_server;
+pub use proto_tower_http_1::server as proto_tower_http_1_server;
 
 #[cfg(feature = "http-2-client")]
-pub use proto_http_2::client as proto_http_2_client;
+pub use proto_tower_http_2::client as proto_tower_http_2_client;
 #[cfg(feature = "http-2-data")]
-pub use proto_http_2::data as proto_http_2_data;
+pub use proto_tower_http_2::data as proto_tower_http_2_data;
 #[cfg(feature = "http-2-server")]
-pub use proto_http_2::server as proto_http_2_server;
+pub use proto_tower_http_2::server as proto_tower_http_2_server;
 
 #[cfg(feature = "http-3-client")]
-pub use proto_http_3::client as proto_http_3_client;
+pub use proto_tower_http_3::client as proto_tower_http_3_client;
 #[cfg(feature = "http-3-data")]
-pub use proto_http_3::data as proto_http_3_data;
+pub use proto_tower_http_3::data as proto_tower_http_3_data;
 #[cfg(feature = "http-3-server")]
-pub use proto_http_3::server as proto_http_3_server;
+pub use proto_tower_http_3::server as proto_tower_http_3_server;
 
 #[cfg(feature = "ice-client")]
-pub use proto_ice::client as proto_ice_client;
+pub use proto_tower_ice::client as proto_tower_ice_client;
 #[cfg(feature = "ice-data")]
-pub use proto_ice::data as proto_ice_data;
+pub use proto_tower_ice::data as proto_tower_ice_data;
 #[cfg(feature = "ice-server")]
-pub use proto_ice::server as proto_ice_server;
+pub use proto_tower_ice::server as proto_tower_ice_server;
 
 #[cfg(feature = "kafka-client")]
-pub use proto_kafka::client as proto_kafka_client;
+pub use proto_tower_kafka::client as proto_tower_kafka_client;
 #[cfg(feature = "kafka-data")]
-pub use proto_kafka::data as proto_kafka_data;
+pub use proto_tower_kafka::data as proto_tower_kafka_data;
 #[cfg(feature = "kafka-server")]
-pub use proto_kafka::server as proto_kafka_server;
+pub use proto_tower_kafka::server as proto_tower_kafka_server;
 
 #[cfg(feature = "mqtt-client")]
-pub use proto_mqtt::client as proto_mqtt_client;
+pub use proto_tower_mqtt::client as proto_tower_mqtt_client;
 #[cfg(feature = "mqtt-data")]
-pub use proto_mqtt::data as proto_mqtt_data;
+pub use proto_tower_mqtt::data as proto_tower_mqtt_data;
 #[cfg(feature = "mqtt-server")]
-pub use proto_mqtt::server as proto_mqtt_server;
+pub use proto_tower_mqtt::server as proto_tower_mqtt_server;
 
 #[cfg(feature = "quic-client")]
-pub use proto_quic::client as proto_quic_client;
+pub use proto_tower_quic::client as proto_tower_quic_client;
 #[cfg(feature = "quic-data")]
-pub use proto_quic::data as proto_quic_data;
+pub use proto_tower_quic::data as proto_tower_quic_data;
 #[cfg(feature = "quic-server")]
-pub use proto_quic::server as proto_quic_server;
+pub use proto_tower_quic::server as proto_tower_quic_server;
 
 #[cfg(feature = "stun-client")]
-pub use proto_stun::client as proto_stun_client;
+pub use proto_tower_stun::client as proto_tower_stun_client;
 #[cfg(feature = "stun-data")]
-pub use proto_stun::data as proto_stun_data;
+pub use proto_tower_stun::data as proto_tower_stun_data;
 #[cfg(feature = "stun-server")]
-pub use proto_stun::server as proto_stun_server;
+pub use proto_tower_stun::server as proto_tower_stun_server;
 
 #[cfg(feature = "tls-client")]
-pub use proto_tls::client as proto_tls_client;
+pub use proto_tower_tls::client as proto_tower_tls_client;
 #[cfg(feature = "tls-data")]
-pub use proto_tls::data as proto_tls_data;
+pub use proto_tower_tls::data as proto_tower_tls_data;
 #[cfg(feature = "tls-server")]
-pub use proto_tls::server as proto_tls_server;
+pub use proto_tower_tls::server as proto_tower_tls_server;
 
 #[cfg(feature = "turn-client")]
-pub use proto_turn::client as proto_turn_client;
+pub use proto_tower_turn::client as proto_tower_turn_client;
 #[cfg(feature = "turn-data")]
-pub use proto_turn::data as proto_turn_data;
+pub use proto_tower_turn::data as proto_tower_turn_data;
 #[cfg(feature = "turn-server")]
-pub use proto_turn::server as proto_turn_server;
+pub use proto_tower_turn::server as proto_tower_turn_server;
 
 #[cfg(feature = "webrtc-client")]
-pub use proto_webrtc::client as proto_webrtc_client;
+pub use proto_tower_webrtc::client as proto_tower_webrtc_client;
 #[cfg(feature = "webrtc-data")]
-pub use proto_webrtc::data as proto_webrtc_data;
+pub use proto_tower_webrtc::data as proto_tower_webrtc_data;
 #[cfg(feature = "webrtc-server")]
-pub use proto_webrtc::server as proto_webrtc_server;
+pub use proto_tower_webrtc::server as proto_tower_webrtc_server;
